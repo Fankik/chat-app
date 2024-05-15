8ea2f59db4b17adffa483d281d102f86816c47e7
Fankik <dima.shibaev@list.ru>
2024-05-16 00:01:50 +0500

    Создано получение списка пользователей

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+/**
+ * Контроллер пользователя
+ */
+class UserController
+{
+    /**
+     * Список пользователей
+     * 
+     * Отдается порционно по 20 пользователей.
+     * Для получения следующих страниц, необходимо передать `page` с номером страницы.
+     *  
+     * @var Request $request
+     * @return \Illuminate\Database\Eloquent\Collection<int, static>
+     *
+     * @queryParam page Номер страницы. Example: 2
+     * @responseFile status=200 scenario="success" storage/responses/users/index.200.json
+     */
+    public function index(Request $request)
+    {
+        $page = $request->input('page') ?? 1;
+        $limit = 20;
+        $skip = ($page - 1) * $limit;
+
+        $users = User::limit($limit)->skip($skip)->get();
+
+        $users->makeHidden(['created_at', 'updated_at']);
+
+        return $users;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    
-    Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
-    Route::post('/login', [AuthController::class, 'index'])->name('auth.index');
 
+    //Регистрация авторизация
+    Route::post('/register', [AuthController::class, 'store'])->name('auth.store');
+    Route::get('/login', [AuthController::class, 'index'])->name('auth.index');
+
+    //Пользователи
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
 });
